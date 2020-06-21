b297354dc26405a8ffa6d1cb8e44cf968c1a17bc
Priyank Patel <priyank89patel@gmail.com>
2020-06-20 22:25:32 -0400

    Initial Commit

---
+++ src/AuthServer/AuthServer.Infrastructure/Data/Identity/AppUser.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AuthServer.Infrastructure.Data.Identity
+{
+    public class AppUser
+    {
+        public string Id { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string NormalizedUserName { get; set; }
+        public string PasswordHash { get; set; }
+    }
+}
