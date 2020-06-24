b12336c3291867705de3347530b84417a76648c5
Priyank Patel <priyank89patel@gmail.com>
2020-06-23 21:01:43 -0400

    IdentityServer4 DbContexts and Migrations

---
+++ src/AuthServer/AuthServer.Infrastructure/Data/IdentityServer/PersistedGrantDb/PersistedGrantDbContextFactory.cs
@@ -0,0 +1,22 @@
+﻿using IdentityServer4.EntityFramework.DbContexts;
+using IdentityServer4.EntityFramework.Options;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace AuthServer.Infrastructure.Data.IdentityServer.PersistedGrantDb
+{
+    public class PersistedGrantDbContextFactory : IDesignTimeDbContextFactory<PersistedGrantDbContext>
+    {
+        public PersistedGrantDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<PersistedGrantDbContext>();
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=PasswordManager_AuthServer;Trusted_Connection=True;MultipleActiveResultSets=true",
+                sql => sql.MigrationsAssembly(typeof(PersistedGrantDbContextFactory).GetTypeInfo().Assembly.GetName().Name));
+            return new PersistedGrantDbContext(optionsBuilder.Options, new OperationalStoreOptions());
+        }
+    }
+}
